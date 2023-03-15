3b66eecd2947ae018b7dd5d47f6949c15d8f75d4
Abiodun-code <olaabiodun157@gmail.com>
2023-03-15 04:02:00 -0700

    photo_third_commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import Header from './components/Header'
 import AnimRoutes from './components/AnimRoutes'
-import { motion } from 'framer-motion'
 
 function App() {
 
--- src/context/CursorContext.jsx
+++ src/context/CursorContext.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, {useState, useEffect, createContext} from 'react'
 
-const CursorContext = () => {
+export const CursorContext = createContext()
+
+const CursorProvider = ({children}) => {
+  const [cursorPos, setCursorPos] = useState({
+    x: 0,
+    y: 0,
+  })
+
+  const [cursorBG, setCursorBG] = useState('default')
+
+  useEffect(()=>{
+  //   const move = (e)=>{
+      
+  //   }
+    window.addEventListener('mousemove',(e)=>{
+      setCursorPos({
+        x: e.clientX,
+        y: e.clientY
+      })
+      return ()=>{
+        window.removeEventListener('mousemove',(e)=>{
+          setCursorPos({
+            x: e.clientX,
+            y: e.clientY
+          })
+        })
+      }
+    })
+  })
+
+  const cursorVariants = {
+    default:{
+      x: cursorPos.x - 16,
+      y: cursorPos.y - 16,
+      backgrounColor: "#0e1112",
+    },
+    text:{
+      width: "150px",
+      height: "150px",
+      x: cursorPos.x - 72,
+      y: cursorPos.y - 72,
+      backgrounColor: "#fff",
+      mixBlendMode: "difference"
+    },
+  }
+
   return (
-    <div>CursorContext</div>
-  )
-}
+    <CursorContext.Provider value={{cursorVariants, cursorBG}}>
+      {children}
+    </CursorContext.Provider>
+  );
+};
 
-export default CursorContext
+export default CursorProvider
